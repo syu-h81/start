bf84818d08bce3244d598846cf827e325b535794
syu danjo <syu56981@gmail.com>
2022-10-19 20:04:11 +0900

    feat: startホームページ作成

---
+++ assets/js/dist/common.dev.js
@@ -0,0 +1,13 @@
+'use strict'; //メニューバーの処理
+
+$(function () {
+  $('#toggle').on('click', function () {
+    $('#toggle').toggleClass('show');
+
+    if ($('.sp-Menu_block').hasClass('slide-R_fadeIn')) {
+      $('.sp-Menu_block').removeClass('slide-R_fadeIn');
+    } else {
+      $('#slide-R').addClass('slide-R_fadeIn');
+    }
+  });
+});
